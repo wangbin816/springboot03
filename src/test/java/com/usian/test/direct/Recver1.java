a58bd5b801a000001113fd7570b0aa86652fc48a
wangbin816 <332545767@qq.com>
2020-11-03 14:35:20 +0800

    改成123123

--- src/test/java/com/usian/test/direct/Recver1.java
+++ src/test/java/com/usian/test/direct/Recver1.java
@@ -17,7 +17,7 @@ public class Recver1 {
         channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,"delete");
         channel.queueBind(QUEUE_NAME,EXCHANGE_NAME,"update");
 
-//123
+//123123
         DefaultConsumer consumer=new DefaultConsumer(channel){
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
